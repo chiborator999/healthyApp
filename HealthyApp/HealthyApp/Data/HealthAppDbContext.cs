aaf11b929b10e7c8a1d572188282538c37af9ba8
Dimitar Spasov <dspasov@zingasoft.com>
2022-03-27 12:50:26 +0300

    feat(healthyApp): dbcontext and migration

---
+++ HealthyApp/HealthyApp/Data/HealthAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthyApp.Data
+{
+    using HealthyApp.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class HealthAppDbContext : IdentityDbContext<User>
+    {
+        public HealthAppDbContext(DbContextOptions<HealthAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

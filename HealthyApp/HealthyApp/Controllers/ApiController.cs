7b6a5fce51b230ad571a690abc61d6a4c3986136
Dimitar Spasov <dspasov@zingasoft.com>
2022-03-27 12:49:31 +0300

    feat(healthyApp): added controllers

---
+++ HealthyApp/HealthyApp/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthyApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")] 
+    public abstract class ApiController : ControllerBase
+    {
+    }
+}
---
+++ HealthyApp/HealthyApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace HealthyApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    public class HomeController : ApiController
+    {
+        [Authorize]
+        public ActionResult Get()
+        {
+            return Ok("Ieaaahhhh");
+        }
+    }
+}

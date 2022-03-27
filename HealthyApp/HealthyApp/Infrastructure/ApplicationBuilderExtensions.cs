a52a00e1025d5afeb8c904e0e3f811886eed9057
Dimitar Spasov <dspasov@zingasoft.com>
2022-03-27 12:51:23 +0300

    feat(healthyApp): added apply migration method

---
+++ HealthyApp/HealthyApp/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HealthyApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthyApp.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder applicationBuilder)
+        {
+            using var services = applicationBuilder.ApplicationServices.CreateScope();
+
+            var dbContext = services.ServiceProvider.GetService<HealthAppDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}

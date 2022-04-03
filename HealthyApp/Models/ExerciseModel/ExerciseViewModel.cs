9d905ad15f7a09b106963c3ab80ced8a06a0d69b
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-03 18:57:41 +0300

    feat(healthyApp): added exercise view and request model

---
+++ HealthyApp/Models/ExerciseModel/ExerciseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthyApp.Models.ExerciseModel
+{
+    public class ExerciseViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string KCalSpent { get; set; }
+    }
+}

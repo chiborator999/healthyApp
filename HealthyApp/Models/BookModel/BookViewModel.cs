a56687c17361a64022af97c3cc71124dd3232e5e
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-03 18:56:50 +0300

    feat(healthyApp): added book view and request model

---
+++ HealthyApp/Models/BookModel/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthyApp.Models.BookModel
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}

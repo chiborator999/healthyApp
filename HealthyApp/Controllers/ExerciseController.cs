69e14aa476347c5d0e04308519bc1aa77c5a5e97
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-03 18:59:03 +0300

    feat(healthyApp): added exercise controller

---
+++ HealthyApp/Controllers/ExerciseController.cs
@@ -0,0 +1,114 @@
+﻿using HealthyApp.Data.Models;
+using HealthyApp.Interfaces;
+using HealthyApp.Models.ExerciseModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyApp.Controllers
+{
+    public class ExerciseController : ApiController
+    {
+        private readonly IExerciseService _exerciseService;
+        private readonly ILogService _logService;
+
+        public ExerciseController(IExerciseService exerciseService, ILogService logService)
+        {
+            _exerciseService = exerciseService;
+            _logService = logService;
+        }
+
+        [HttpPost]
+        [Route(nameof(Create))]
+        public async Task<IActionResult> Create([FromBody] ExerciseRequestModel exerciseModel)
+        {
+            try
+            {
+                var exercise = new Exercise
+                {
+                    Name = exerciseModel.Name,
+                    KCalSpent = exerciseModel.KCalSpent
+                };
+
+                await _exerciseService.CreateExerciseAsync(exercise);
+
+                return Ok($"Successfuly created Exercise with id: {exercise.Id}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(await _logService.LogExceptionAsync(ex));
+            };
+        }
+
+        [HttpPut]
+        [Route(nameof(Update))]
+        public async Task<IActionResult> Update([FromBody] ExerciseRequestModel exerciseModel)
+        {
+            try
+            {
+                var exercise = await _exerciseService.GetExerciseByIdAsync(exerciseModel.Id);
+
+                if(exercise == null)
+                {
+                    return BadRequest($"Exercise with id: {exerciseModel.Id} can not be found!");
+                }
+
+                var updatedExercise = new Exercise
+                {
+                    Id = exercise.Id,
+                    Name = exerciseModel.Name,
+                    KCalSpent = exerciseModel.KCalSpent
+                };
+
+                await _exerciseService.UpdateExerciseAsync(updatedExercise);
+
+                return Ok($"Successfuly updated Exercise with id: {exercise.Id}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(await _logService.LogExceptionAsync(ex));
+            };
+        }
+
+        [HttpDelete]
+        [Route(nameof(Remove))]
+        public async Task<IActionResult> Remove(int exerciseId)
+        {
+            try
+            {
+                await _exerciseService.RemoveExerciseAsync(exerciseId);
+                return Ok($"Successfuly remove Exercise with id: {exerciseId}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(await _logService.LogExceptionAsync(ex));
+            }
+        }
+
+        [HttpGet]
+        [Route(nameof(GetById))]
+        public async Task<IActionResult> GetById(int exerciseId)
+        {
+            try
+            {
+                return Ok(await _exerciseService.GetExerciseByIdAsync(exerciseId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(await _logService.LogExceptionAsync(ex));
+            }
+        }
+
+        [HttpGet]
+        [Route(nameof(GetAll))]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _exerciseService.GetAllExercisesAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(await _logService.LogExceptionAsync(ex));
+            }
+        }
+    }
+}

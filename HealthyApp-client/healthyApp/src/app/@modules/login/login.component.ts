9e5a59e17b7cb0894b0498213b629636c3992312
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-03 23:42:59 +0300

    feat(healthyApp-client): updated register component

--- HealthyApp-client/healthyApp/src/app/@modules/login/login.component.ts
+++ HealthyApp-client/healthyApp/src/app/@modules/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/@shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authServise: AuthService,
+    ) { 
+    this.loginForm = this.fb.group({
+      'username': ['', [Validators.required]],
+      'password': ['', [Validators.required]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.authServise.login(this.loginForm.value).subscribe(response =>{
+      console.log(response);
+    //   this.snackbar.open(`${response}`, 'X', {
+    //     horizontalPosition: 'center',
+    //     verticalPosition: 'bottom',
+    //     panelClass: 'successSnackbar'
+    //   });
+    //   this.loading = false;
+    // }, error => {
+    //     // this.errorHandlerService.handleRequestError(error);
+    //     this.loading = false;
+    // });
+      }
+  )};
 }
--- HealthyApp-client/healthyApp/src/app/@modules/register/register.component.ts
+++ HealthyApp-client/healthyApp/src/app/@modules/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/@shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+    ) { 
+    this.registerForm = this.fb.group({
+      'username': ['', [Validators.required]],
+      'password': ['', [Validators.required]],
+      'email': ['', [Validators.required]],
+      'firstName': ['', [Validators.required]],
+      'lastName': ['', [Validators.required]],
+      'age': ['', [Validators.required]],
+      'gender': ['', [Validators.required]],
+      'weight': ['', [Validators.required]],
+      'height': ['', [Validators.required]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  register() {
+    this.authService.register(this.registerForm.value).subscribe(response =>{
+      console.log(response);
+    })
+  }
 }

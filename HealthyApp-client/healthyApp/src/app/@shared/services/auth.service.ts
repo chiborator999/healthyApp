eeeecb49d0b2e697ae4871094d51fbb6e3ff7b69
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-05 00:21:24 +0300

    feat(healthyApp):  updated auth service

--- HealthyApp-client/healthyApp/src/app/@shared/services/auth.service.ts
+++ HealthyApp-client/healthyApp/src/app/@shared/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -7,11 +7,20 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
+  headers: HttpHeaders;
 
   private loginPath = environment.apiUrl + '/Identity/Login';
   private registerPath = environment.apiUrl + '/Identity/Register';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    let authToken = sessionStorage.getItem('token');
+    this.headers =  new HttpHeaders()
+    .set('Accept', 'application/json')
+    .set('Access-Control-Allow-Headers', 'Content-Type')
+    .set('Access-Control-Allow-Methods', ['GET', 'PUT', 'POST', 'DELETE'])
+    .set('Access-Control-Allow-Origin', '*')
+    .set('Authorization', 'Bearer ' + authToken);
+  }
 
   login(data: any): Observable<any> {
     return this.http.post(this.loginPath, data);
@@ -19,5 +28,20 @@ export class AuthService {
 
   register(data: any): Observable<any> {
     return this.http.post(this.registerPath, data);
+  }
+
+  saveToken(token: any) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated() {
+    if (this.getToken()) {
+      return true;
+    }
+    return false;
   }
 }

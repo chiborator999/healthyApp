9abc690d0f4e406a7d7a6bfba214bdeda486271f
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-10 11:23:21 +0300

    refactor(healthyApp-client): auth service

--- HealthyApp-client/healthyApp/src/app/@shared/services/auth.service.ts
+++ HealthyApp-client/healthyApp/src/app/@shared/services/auth.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
+import { EventEmitter, Input, Output } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  @Output() isUserLoggedIn = new EventEmitter<boolean>();
   headers: HttpHeaders;
+  accessToken = 'token';
 
   private loginPath = environment.apiUrl + '/Identity/Login';
   private registerPath = environment.apiUrl + '/Identity/Register';
 
   constructor(private http: HttpClient) { 
-    let authToken = sessionStorage.getItem('token');
+    let authToken = sessionStorage.getItem(this.accessToken);
     this.headers =  new HttpHeaders()
     .set('Accept', 'application/json')
     .set('Access-Control-Allow-Headers', 'Content-Type')
@@ -21,7 +25,6 @@ export class AuthService {
     .set('Access-Control-Allow-Origin', '*')
     .set('Authorization', 'Bearer ' + authToken);
   }
-
   login(data: any): Observable<any> {
     return this.http.post(this.loginPath, data);
   }
@@ -30,16 +33,32 @@ export class AuthService {
     return this.http.post(this.registerPath, data);
   }
 
+  logout() {
+    localStorage.removeItem(this.accessToken);
+    localStorage.removeItem('isLoggedIn');
+  }
+
   saveToken(token: any) {
-    localStorage.setItem('token', token);
+    localStorage.setItem(this.accessToken, token);
+    localStorage.setItem("isLoggedIn", "true");
+    // sessionStorage.setItem(this.accessToken, token);
   }
 
   getToken() {
-    return localStorage.getItem('token');
+    return localStorage.getItem(this.accessToken);
+  }
+
+  getUserData(): void {
+    if (this.isAuthenticated()) {
+      let token = localStorage.getItem(this.accessToken);
+      let decodedToken = jwt_decode(`${token}`);
+      console.log(decodedToken)
+    }
   }
 
   isAuthenticated() {
     if (this.getToken()) {
+      this.isUserLoggedIn.emit(true);
       return true;
     }
     return false;

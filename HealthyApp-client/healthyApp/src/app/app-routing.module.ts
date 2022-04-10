a6b3fdbe29cdb6469f7c939b37cf695776528555
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-10 11:19:49 +0300

    refactor(healthyApp-client): app component

--- HealthyApp-client/healthyApp/src/app/app-routing.module.ts
+++ HealthyApp-client/healthyApp/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './@modules/book/book.component';
+import { FooterComponent } from './@modules/footer/footer.component';
 import { HomeComponent } from './@modules/home/home.component';
 import { LoginComponent } from './@modules/login/login.component';
 import { RegisterComponent } from './@modules/register/register.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  // { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }
+  // { path: 'home', component: HomeComponent },
+  { path: 'book', component: BookComponent }
   //  path: 'createMeal', component: HomeComponent, canActivate: [AuthGuardService]}
 ];
 
--- HealthyApp-client/healthyApp/src/app/app.module.ts
+++ HealthyApp-client/healthyApp/src/app/app.module.ts
@@ -1,38 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './@modules/home/home.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './@modules/login/login.component';
 import { RegisterComponent } from './@modules/register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './@shared/services/auth.service';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { AuthGuardService } from './@shared/services/auth-guard.service';
-import { TokenInterceptorService } from './@shared/services/token-interceptor.service';
+import { ProductService } from './@shared/services/product.service';
+import { MealService } from './@shared/services/meal.service';
+import { BookService } from './@shared/services/book.service';
+import { ExerciseService } from './@shared/services/exercise.service';
+import { BookComponent } from './@modules/book/book.component';
+import { AngularMaterialModule } from './core/angular-material.module';
+import { MatTableModule } from '@angular/material/table'
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './@modules/footer/footer.component';
+import { ErrorHandlerService } from './@shared/services/error-handler.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    BookComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [
     AuthService, 
     AuthGuardService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptorService,
-      multi: true
-    }
+    ProductService,
+    MealService,
+    BookService,
+    ExerciseService,
+    ErrorHandlerService
   ],
   bootstrap: [AppComponent]
 })

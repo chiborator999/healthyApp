a63d506a472b1a19cbb0c3d08c353e0298ad9420
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-10 13:49:53 +0300

    feat(healthyApp-client): added meal component

---
+++ HealthyApp-client/healthyApp/src/app/@modules/meal/meal.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router'; 
+import { MatSort } from '@angular/material/sort';
+import { ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/@shared/services/auth.service';
+import { ErrorHandlerService } from 'src/app/@shared/services/error-handler.service';
+import { MealService } from 'src/app/@shared/services/meal.service';
+import { MealResponseModule } from 'src/app/@shared/models/response/meal-response/meal-response.module';
+import { ProductResponseModule } from 'src/app/@shared/models/response/product-response/product-response.module';
+
+@Component({
+  selector: 'app-meal',
+  templateUrl: './meal.component.html',
+  styleUrls: ['./meal.component.scss']
+})
+export class MealComponent implements OnInit {
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['mealType', 'mealCategory', 'products', 'action'];
+  loading: boolean = false;
+  meals: MealResponseModule[];
+  products: any// ProductResponseModule[];
+  isUserLoggedIns: boolean;
+
+
+  @ViewChild(MatSort) sort!: MatSort;;
+  
+  
+  constructor(
+    private mealService: MealService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private authService: AuthService,
+    private errorHandlerService: ErrorHandlerService,   
+    
+    ) { }
+    
+    ngOnInit(): void {
+      this.getData();
+  }
+
+getProductsName(products: Array<any>) {
+ let result = products.map(p => p.name);
+ return result;
+}  
+
+getMealCategory(enumTypeNumber: any): string {
+  let category = "";
+  switch(enumTypeNumber) {
+    case 0: 
+      category = "Vegan";
+      break; 
+    case 1: 
+      category = "Vegetarian";
+      break;
+    case 2: 
+      category = "Pescetarian";
+      break;
+    case 3: 
+      category = "Meat";
+      break;
+  }
+return category;
+}
+
+getMealType(enumTypeNumber: any): string {
+  let type = "";
+  switch(enumTypeNumber) {
+    case 0: 
+      type = "Breakfast";
+      break; 
+    case 1: 
+      type = "Lunch";
+      break;
+    case 2: 
+      type = "Dinner";
+      break;
+  }
+ return type;
+}
+
+sortDataAccsesor(item: any, property: any){
+  switch (property) { 
+    case 'title': return item.title.toUpperCase();
+    case 'author': return item.title.toUpperCase();
+    case 'url': return item.title.toUpperCase();
+    default: return item[property];
+  }
+}
+
+getData(){
+    this.mealService.getAll().subscribe(response => {
+      this.meals = response as MealResponseModule[]; 
+      this.dataSource = new MatTableDataSource(this.meals);
+      this.dataSource.sort = this.sort;
+      this.dataSource.sortingDataAccessor = (item, property) => this.sortDataAccsesor(item, property);  
+      this.loading = false;
+    })          
+
+    // },
+    // error => {
+    //   this.errorHandler.handleRequestError(error);
+    //   this.loading = false;
+    // });
+}
+
+add() {
+  console.log("add");
+}
+
+edit(element: any) {
+  console.log("edit");
+}
+
+  // add(){
+  //   this.router.navigate(['meal/add']);
+  // }
+
+  // edit(element: any){
+  //   this.router.navigate(['meal/edit', element.id]);
+  // }
+  
+  delete(element:any){
+    let result = confirm(`You are about to delete ${element.title}\n\n Are you sure?`);
+    if(result){
+      this.mealService.remove(element.id).subscribe({
+        next: () => {
+          this.getData();
+          this.snackbar.open(`successful delete meal with id: ${element.id}`, 'X', {
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom',
+            panelClass: 'successSnackbar'
+          });
+        },
+        error: error => {
+          this.errorHandler.handleRequestError(error);
+          console.log(error)
+        },
+        complete: () => {      
+        }
+    })}
+  }
+}

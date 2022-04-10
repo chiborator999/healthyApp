eb0a6a13abebbfd77a7de09859bdcfa0aa1be477
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-10 13:47:39 +0300

    refactor(healthyApp-client): app-routing module

--- HealthyApp-client/healthyApp/src/app/app-routing.module.ts
+++ HealthyApp-client/healthyApp/src/app/app-routing.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './@modules/book/book.component';
-import { FooterComponent } from './@modules/footer/footer.component';
-import { HomeComponent } from './@modules/home/home.component';
+import { DashboardComponent } from './@modules/dashboard/dashboard.component';
+import { ExerciseComponent } from './@modules/exercise/exercise.component';
 import { LoginComponent } from './@modules/login/login.component';
+import { MealComponent } from './@modules/meal/meal.component';
+import { ProfileComponent } from './@modules/profile/profile.component';
 import { RegisterComponent } from './@modules/register/register.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  // { path: '', component: AppComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // { path: 'home', component: HomeComponent },
-  { path: 'book', component: BookComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'meal', component: MealComponent },
+  { path: 'exercise', component: ExerciseComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
   //  path: 'createMeal', component: HomeComponent, canActivate: [AuthGuardService]}
 ];
 

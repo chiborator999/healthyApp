fb950c00d0ca25e7bd5676cc20ef2bc3a5aa5df3
Dimitar Spasov <dspasov@zingasoft.com>
2022-04-11 23:56:19 +0300

    refactor(healthyApp): meal service

--- HealthyApp/Interfaces/IMealService.cs
+++ HealthyApp/Interfaces/IMealService.cs
@@ -7,10 +7,6 @@
     {
         Task<IEnumerable<MealViewModel>> GetAllMealsAsync();
         Task<MealViewModel> GetMealByIdAsync(int mealId);
-        Task<double> GetMealKCalByIdAsync(int mealId);
-        Task<double> GetFatKCalByIdAsync(int mealId);
-        Task<double> GetProteinKCalByIdAsync(int mealId);
-        Task<double> GetCarbohydrateKCalByIdAsync(int mealId);
         Task CreateMealAsync(Meal meal);
         Task UpdateMealAsync(Meal meal);
         Task RemoveMealAsync(int mealId);
